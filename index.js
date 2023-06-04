8dd55847dd966388b30f23ccf90785c78e9f6dea
Shubham Sharma <85790050+shubham199927@users.noreply.github.com>
2023-06-05 02:26:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var countr = document.querySelectorAll(".drum").length;
 for(var i =0; i<countr;i++){
 
 document.querySelectorAll(".drum")[i].addEventListener("click", aclick);
+document.addEventListener("keydown",press);
 }
 function aclick(){
 var textof = this.innerHTML;
@@ -35,15 +36,47 @@ switch (textof) {
                                sanre.play();
                         break;        
     default:
+        alert("Press onto boxes");
         break;
 }
 
 }
+function press(f)
+{
+var x = f.key;
+switch (x) {
+    case "w":
+        var kick = new Audio("./sounds/kick-bass.mp3");
+               kick.play();
+        break;
+        case "a":
+        var sanre = new Audio("./sounds/snare.mp3");
+               sanre.play();
+        break;
+        case "s":
+            var sanre = new Audio("./sounds/tom-1.mp3");
+                   sanre.play();
+            break;
+       case "d":
+                var sanre = new Audio("./sounds/crash.mp3");
+                       sanre.play();
+                break; 
+        case "j":
+                var sanre = new Audio("./sounds/tom-2.mp3");
+                       sanre.play();
+                break;  
+        case "k":
+                    var sanre = new Audio("./sounds/tom-3.mp3");
+                           sanre.play();
+                    break;          
+         case "l":
+                        var sanre = new Audio("./sounds/tom-4.mp3");
+                               sanre.play();
+                        break;        
+    default:
+        alert("Press the right keys");
+        break;
+}
 
-
-
-
-
-
-
+}
 
